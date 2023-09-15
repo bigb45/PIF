e7fad9708a125b6253c2554beca173c8e7e1f457
wasem-ibrahim <wasem.ibrahim98@gmail.com>
2023-09-15 22:48:27 +0300

    forgot what i changed

--- src/components/ui/Message.jsx
+++ src/components/ui/Message.jsx
@@ -8,8 +8,7 @@ const Message = ({ role, content }) => {
     <div
       className={`flex space-x-4 items-center p-4 ${
         isUserMessage ? "justify-end" : ""
-      } `}
-    >
+      } `}>
       {isUserMessage ? null : (
         <Image
           className="rounded-sm"
@@ -24,16 +23,14 @@ const Message = ({ role, content }) => {
           isUserMessage
             ? "items-end ml-24 bg-[#202123] text-white"
             : "items-start mr-24 bg-gray-300"
-        }   rounded-lg p-2 m-4 hover:shadow-lg transition`}
-      >
-        <ReactMarkdown
+        }   rounded-lg p-2 m-4 hover:shadow-lg transition`}>
+        <p
           className={`${
             isUserMessage ? "text-white" : "text-zinc-400"
-          } text-sm mb-1 first-letter:uppercase`}
-        >
+          } text-sm mb-1 first-letter:uppercase`}>
           {isUserMessage ? null : role}
-        </ReactMarkdown>
-        <p className="text-sm">{content}</p>
+        </p>
+        <ReactMarkdown className="text-sm">{content}</ReactMarkdown>
       </div>
     </div>
   );
